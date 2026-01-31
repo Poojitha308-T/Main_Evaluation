91abee94ef4f6520cfcb375e8bf16a1003c4be27
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-31 20:33:16 +0530

    Fleet-management-app

--- Q1/src/controllers/trip.controller.js
+++ Q1/src/controllers/trip.controller.js
@@ -28,4 +28,54 @@ export const createTrip = async(req,res) => {
     res.json(data[0]);
 }
 
+export const getTrip = async(req,res) =>{
+    const {data} = await supabase
+    .from("trips")
+    .select("*")
+    .eq("id",req.params.tripId)
+    .single();
 
+    res.json(data);
+}
+
+export const updateTrip = async(req,res) => {
+    await supabase.from("trips")
+    .update(req.body)
+    .eq("id",req.params.tripId);
+    res.json({message:"Trip updated"});
+}
+
+export const deleteTrip = async(req,res) => {
+    await supabase.from("trips")
+    .delete()
+    .eq("id", req.params.tripId);
+    res.json({message:"Trip deleted"})
+}
+
+export const endTrip = async(req,res) => {
+    const {data:trip} = await supabase
+    .from("trips")
+    .select("*")
+    .eq("id",req.params.tripId)
+    .single();
+
+    const {data: vehicle} = await supabase
+    .from("vehicles")
+    .select("*")
+    .eq("id", trip.vehicle_id)
+    .single();
+
+    const tripCost = trip.distance_km * vehicle.rate_per_km;
+
+    await supabase
+    .from("trips")
+    .update({iscomplete: true, tripcost: tripCost, end_date: new Date()})
+    .eq("id",trip.id);
+
+    await supabase
+    .from("vehicles")
+    .update({isavailable:true})
+    .eq("id",vehicle.id);
+
+    res.json({message:"Trip ended", tripCost});
+}
